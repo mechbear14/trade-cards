d8e581a3fd65fb8a29979e3c7bfe1af37c99fe8e
Gabe Lyu <no6xichengavenue@gmail.com>
2020-05-15 15:20:58 +0100

    Initial clean-up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      message: "Roar! You've found me!"
+    };
+  }
+  render(){
+    return(
+      <h1>{this.props.name} says {this.state.message}</h1>
+    );
+  }
 }
 
 export default App;
